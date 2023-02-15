cdf3485a8816838189478e9e06c5d1f0bf813f04
Amaia Serrano Padrones <amaiaserranop@gmail.com>
2023-02-15 20:10:29 +0100

    First assignment finished

---
+++ Assignment1/StoneMasonKarel.java
@@ -0,0 +1,54 @@
+/*
+ * File: StoneMasonKarel.java
+ * --------------------------
+ * The StoneMasonKarel subclass as it appears here does nothing.
+ * When you finish writing it, it should solve the "repair the quad"
+ * problem from Assignment 1.  In addition to editing the program,
+ * you should be sure to edit this comment so that it no longer
+ * indicates that the program does nothing.
+ */
+
+import stanford.karel.*;
+
+public class StoneMasonKarel extends SuperKarel {
+
+	// You fill in this part
+	public void run(){
+		goUpPuttingStones();
+		goBackDown();
+		while(frontIsClear()){
+			move4Avenues();
+			goUpPuttingStones();
+			goBackDown();
+		}
+
+	}
+	
+	private void goUpPuttingStones(){
+		turnLeft();
+		while(frontIsClear()){
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+			move();
+		}
+		if(noBeepersPresent()){
+			putBeeper();
+		}
+	}
+	
+	private void goBackDown(){
+		turnLeft();
+		turnLeft();
+		while(frontIsClear()){
+			move();
+		}
+		turnLeft();
+	}
+	
+	private void move4Avenues(){
+		for(int i = 0; i < 4; i++){
+			move();			
+		}
+	}
+}
